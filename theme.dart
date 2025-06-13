0091b46625c42a46cdcd19e0ca7fa8306ae0dd0a
AdrVas89 <adrvas3@gmail.com>
2025-06-13 14:28:39 +0300

    Create theme.dart

---
+++ theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(
+  primarySwatch: Colors.indigo,
+  appBarTheme: AppBarTheme(color: Colors.indigo),
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+);
