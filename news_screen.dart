e189eb65345208192afb3785c351c4ee36981942
AdrVas89 <adrvas3@gmail.com>
2025-06-13 14:39:19 +0300

    Create news_screen.dart

---
+++ news_screen.dart
@@ -0,0 +1,12 @@
+// screens/news_screen.dart
+import 'package:flutter/material.dart';
+
+class NewsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Market News')),
+      body: Center(child: Text('Latest market news coming soon...')),
+    );
+  }
+}
