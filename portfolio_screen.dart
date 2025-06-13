1898577396e46cc7daf1690246f0dc03b44646e3
AdrVas89 <adrvas3@gmail.com>
2025-06-13 14:41:12 +0300

    Create portfolio_screen.dart

---
+++ portfolio_screen.dart
@@ -0,0 +1,12 @@
+// screens/portfolio_screen.dart
+import 'package:flutter/material.dart';
+
+class PortfolioScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My Portfolio')),
+      body: Center(child: Text('Track your portfolio here...')),
+    );
+  }
+}
